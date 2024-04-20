3100ee8f2548c01aa9118183a8f3db350ea77cd5
Ildar Meyker <ildar.meyker@gmail.com>
2024-04-20 23:01:54 +0300

    Update.

--- bin/exec.js
+++ bin/exec.js
@@ -3,7 +3,7 @@
 const { exec } = require("child_process");
 
 exec(
-	"cd ./node_modules/test-package && npm install && npx webpack --config ./webpack.config.js && cp -r ./build-view ../../view/test-package-build/home/",
+	"cd ./node_modules/test-package && npm install && npx webpack --config ./webpack.config.js && mkdir ../../view/test-package-build &&  cp -r ./build-view ../../view/test-package-build/home/",
 	(error, stdout, stderr) => {
 		if (error) {
 			console.log(`error: ${error.message}`);
