eaad6c4e37d28b5d600ca29ecc8cbb5e348c6071
Ildar Meyker <ildar.meyker@gmail.com>
2024-04-20 22:56:29 +0300

    Update.

--- bin/exec.js
+++ bin/exec.js
@@ -2,14 +2,17 @@
 
 const { exec } = require("child_process");
 
-exec("pwd .", (error, stdout, stderr) => {
-	if (error) {
-		console.log(`error: ${error.message}`);
-		return;
+exec(
+	"cd ./node_modules/test-package && npm install && webpack --config ./webpack.config.js && cp -r ./build-view ../../view/test-package-build/home/",
+	(error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
 	}
-	if (stderr) {
-		console.log(`stderr: ${stderr}`);
-		return;
-	}
-	console.log(`stdout: ${stdout}`);
-});
+);
