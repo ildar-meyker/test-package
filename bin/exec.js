4ce72e77bfed3cfa92c53146718eab2a37e8273d
Ildar Meyker <ildar.meyker@gmail.com>
2024-04-20 23:16:39 +0300

    Update.

--- bin/exec.js
+++ bin/exec.js
@@ -2,40 +2,22 @@
 
 const { exec } = require("child_process");
 
-// Команды для выполнения
-const commands = [
-	"cd ./node_modules/test-package",
-	"npm install",
-	"npx webpack --config ./webpack.config.js",
-	"mkdir -p ../../views/test-package-build",
-	"cp -r ./build-view ../../views/test-package-build/home/",
-];
+const command = `
+  cd ./node_modules/test-package &&
+  npm install &&
+  npx webpack --config ./webpack.config.js &&
+  mkdir -p ../../views/test-package-build &&
+  cp -r ./build-view ../../views/test-package-build/home/
+`;
 
-// Функция для последовательного выполнения команд
-function executeCommands(index) {
-	if (index >= commands.length) {
-		console.log("Все команды выполнены успешно!");
+exec(command, (error, stdout, stderr) => {
+	if (error) {
+		console.error(`Ошибка: ${error.message}`);
 		return;
 	}
-
-	const command = commands[index];
-	console.log(`Выполнение команды: ${command}`);
-
-	exec(command, (error, stdout, stderr) => {
-		if (error) {
-			console.error(`Ошибка: ${error.message}`);
-			return;
-		}
-		if (stderr) {
-			console.error(`Ошибка вывода: ${stderr}`);
-			return;
-		}
-		console.log(`Результат: ${stdout}`);
-
-		// Переход к следующей команде
-		executeCommands(index + 1);
-	});
-}
-
-// Начало выполнения команд
-executeCommands(0);
+	if (stderr) {
+		console.error(`Ошибка вывода: ${stderr}`);
+		return;
+	}
+	console.log(`Результат: ${stdout}`);
+});
