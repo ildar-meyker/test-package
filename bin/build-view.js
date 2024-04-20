fc3feb7f7f4dd09da368bc2643bca1eaf788bcb3
Ildar Meyker <ildar.meyker@gmail.com>
2024-04-20 20:10:52 +0300

    Update.

---
+++ bin/build-view.js
@@ -0,0 +1,30 @@
+// run webpack and create build
+
+const config = require("../webpack.config.js");
+const webpack = require("webpack");
+
+const compiler = webpack(config);
+
+compiler.run((err, stats) => {
+	if (err) {
+		console.error(err.stack || err);
+		if (err.details) {
+			console.error(err.details);
+		}
+		return;
+	}
+
+	const info = stats.toJson();
+
+	if (stats.hasErrors()) {
+		console.error(info.errors);
+	}
+
+	if (stats.hasWarnings()) {
+		console.warn(info.warnings);
+	}
+
+	compiler.close((closeErr) => {
+		// ...
+	});
+});
